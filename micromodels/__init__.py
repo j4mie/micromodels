f37486284c9386bb022eb6ed127989c933393cfe
Jamie Matthews <jamie.matthews@gmail.com>
2010-12-28 15:29:09 +0000

    Add setup.py and related stuff

--- micromodels/__init__.py
+++ micromodels/__init__.py
@@ -1,3 +1,5 @@
 from models import Model
 from fields import PassField, CharField, IntegerField, BooleanField, DateTimeField, DateField, \
                    TimeField, ModelField, ModelCollectionField, FieldCollectionField
+
+__version__ = '0.0.1'
---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+import re
+from distutils.core import setup
+
+rel_file = lambda *args: os.path.join(os.path.dirname(os.path.abspath(__file__)), *args)
+
+def read_from(filename):
+    fp = open(filename)
+    try:
+        return fp.read()
+    finally:
+        fp.close()
+
+def get_long_description():
+    return read_from(rel_file('README.md'))
+
+def get_version():
+    data = read_from(rel_file('micromodels/__init__.py'))
+    return re.search(r"__version__ = '([^']+)'", data).group(1)
+
+setup(
+    name='micromodels',
+    description='Declarative dictionary-based read-only model classes for Python',
+    long_description=get_long_description(),
+    version=get_version(),
+    packages=['micromodels'],
+    url='https://github.com/j4mie/micromodels/',
+    author='Jamie Matthews',
+    author_email='jamie.matthews@gmail.com',
+    license='Public Domain',
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
