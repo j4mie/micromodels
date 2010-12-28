3eadc66259e072d66021bcd6ed1a9ee8797952df
Jamie Matthews <jamie.matthews@gmail.com>
2010-12-28 15:42:13 +0000

    Typo in version

--- micromodels/__init__.py
+++ micromodels/__init__.py
@@ -2,4 +2,4 @@ from models import Model
 from fields import PassField, CharField, IntegerField, BooleanField, DateTimeField, DateField, \
                    TimeField, ModelField, ModelCollectionField, FieldCollectionField
 
-__version__ = '0.0.1'
+__version__ = '0.1.0'
