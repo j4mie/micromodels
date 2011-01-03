173442da3e9f75f7e68637428a7bce20ea508e24
Eric Martin <e.a.martin1337@gmail.com>
2011-01-03 19:47:17 +0000

    Switched to dot imports for local imports.

--- micromodels/__init__.py
+++ micromodels/__init__.py
@@ -1,5 +1,5 @@
-from models import Model, JSONModel
-from fields import PassField, CharField, IntegerField, BooleanField, DateTimeField, DateField, \
+from .models import Model, JSONModel
+from .fields import PassField, CharField, IntegerField, BooleanField, DateTimeField, DateField, \
                    TimeField, ModelField, ModelCollectionField, FieldCollectionField
 
 __version__ = '0.1.0'
