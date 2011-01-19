743227b288a0e832f1683398a417571c866ffee2
Jamie Matthews <jamie.matthews@gmail.com>
2011-01-19 20:32:26 +0000

    Bump version

--- micromodels/__init__.py
+++ micromodels/__init__.py
@@ -3,4 +3,4 @@ from .fields import BaseField, CharField, IntegerField, BooleanField,\
                     DateTimeField, DateField, TimeField, ModelField,\
                     ModelCollectionField, FieldCollectionField
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
