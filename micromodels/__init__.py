dfa6ca297c03c866ce04bfcc3d1d058fa09288e5
Eric Moritz <eric@themoritzfamily.com>
2012-08-01 12:20:00 -0400

    bumped version to 0.5.1

--- micromodels/__init__.py
+++ micromodels/__init__.py
@@ -3,4 +3,4 @@ from .fields import BaseField, CharField, IntegerField, FloatField,\
                     BooleanField, DateTimeField, DateField, TimeField,\
                     ModelField, ModelCollectionField, FieldCollectionField
 
-__version__ = '0.5.0'
+__version__ = '0.5.1'
